638f5e1346a04b99dda144eaa5a9ca01aa495fcd
Emmanuel Egomson <fegomson@gmail.com>
2025-12-23 16:50:33 -0600

    feat: add MongoDB connection and user/post models, update dependencies, and create environment configuration

---
+++ src/models/Post.ts
@@ -0,0 +1,66 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+enum PostStatus {
+    DRAFT = 'DRAFT',
+    PUBLISHED = 'PUBLISHED'
+}
+
+interface Post extends Document {
+    id: string
+    title: string
+    slug: string
+    content: string
+    status: PostStatus
+    seo: {
+        title: string
+        description: string
+        keywords: string[]
+    }
+    author: string
+}
+
+const postSchema = new Schema({
+    title: { 
+        type: String, 
+        required: true 
+    },
+    slug: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    content: { 
+        type: String, 
+        required: true 
+    },
+    status: { 
+        type: String, 
+        enum: Object.values(PostStatus), 
+        required: true 
+    },
+    seo: {
+        title: { 
+            type: String, 
+            required: true 
+        },
+        description: { 
+            type: String, 
+            required: true 
+        },
+        keywords: { 
+            type: [String], 
+            required: true 
+        }
+    },  
+    author: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true 
+    }
+}, {
+    timestamps: true
+})
+
+const Post = mongoose.model<Post>('Post', postSchema);
+
+export default Post;
